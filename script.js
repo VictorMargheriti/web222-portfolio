da5d50240e7e7a73c514bcdd0b63eb49ea3e6d14
Victor <64934876+VictorMargheriti@users.noreply.github.com>
2021-12-12 01:25:46 -0500

    create contact page/mobile responsiveness

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 AOS.init();
+let myVar;
+
+const myFunction = () => {
+  myVar = setTimeout(showPage, 1500);
+}
+
+const showPage = () => {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+const hamburgerMenu = () => {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
